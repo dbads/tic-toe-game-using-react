ec2c749af9f58f0cb19f4eb405ba010930de6555
Deepak Bharti <deepakbharti823@gmail.com>
2019-03-24 23:50:54 +0530

    improving tic-tac-to

--- src/App.js
+++ src/App.js
@@ -75,14 +75,14 @@ class Game extends React.Component {
 
   render() {
     const history = this.state.history;
-    const current = history[history.length-1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step,move)=>{
         const desc = move ? "Go to move # "+ move : "Go to game start.";
         return(
           <li key={move}>
-            <button onClick={()=>this.jumpTo()}>
+            <button onClick={()=>this.jumpTo(move)}>
               {desc}
             </button>
           </li>
